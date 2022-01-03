2c1de3f4e7f7a883acd302574edd341f5472ebe6
Sebastian Kaspari <s.kaspari@gmail.com>
2022-01-03 16:43:06 +0100

    Defining tokens

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pocmo/monkey-go
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pocmo/monkey-go/token"
+)
+
+func main() {
+	fmt.Println("Token: " + token.EOF)
+}
